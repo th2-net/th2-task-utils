0c252e33fe380c18da1482bcaeadb961d8e2e50a
Nikita Smirnov <46124551+Nikita-Smirnov-Exactpro@users.noreply.github.com>
2025-03-24 19:26:03 +0400

    Update src/main/java/com/exactpro/th2/taskutils/FutureTracker.java
    Co-authored-by: Oleg Smirnov <smirnov_oleg96@mail.ru>

--- src/main/java/com/exactpro/th2/taskutils/FutureTracker.java
+++ src/main/java/com/exactpro/th2/taskutils/FutureTracker.java
@@ -96,7 +96,7 @@ public class FutureTracker<T> {
      */
     public boolean track(CompletableFuture<T> future) throws InterruptedException {
         while (!track(future, DURATION_MINUTE)) {
-            LOGGER.warn("Future can't be tracked longer than {}", DURATION_MINUTE);
+            LOGGER.warn("Future can't be added to track list for longer than {}", DURATION_MINUTE);
             if (Thread.currentThread().isInterrupted()) { // Clears interrupted status!
                 throw new InterruptedException();
             }
